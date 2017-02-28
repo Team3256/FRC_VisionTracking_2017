930700edc16ba52a0cbe42593dc5a936762c72e3
Bo887 <ezhang887@gmail.com>
2017-02-27 17:46:26 -0800

    Add comments

--- start_vision_tracking.sh
+++ start_vision_tracking.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+#This lowers the camera exposure
 v4l2-ctl -c exposure_auto=1 -c exposure_absolute=10 -d /dev/video1
+
+#This starts up the vision tracking
 python gear_lift_tracking.py
